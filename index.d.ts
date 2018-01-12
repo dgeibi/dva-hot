857cba589e002e761858966a5a2074e7bca0b96b
dgeibi <png.inside@gmail.com>
2018-01-12 13:15:43 +0800

    (d.ts): rename var

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,13 @@
 import { Model, DvaInstance, Router } from './dva'
 
-type AcceptModel = (model: Model) => Model
-type AcceptRouter = (router: Router) => Router
+type ModelWrapper = (model: Model) => Model
+type RouterWrapper = (router: Router) => Router
 interface NodeModule extends __WebpackModuleApi.Module {}
-interface dvaHot {
+interface DvaHot {
   patch(app: DvaInstance): DvaInstance
-  model(srcModule: NodeModule): AcceptModel
-  router(srcModule: NodeModule): AcceptRouter
+  model(srcModule: NodeModule): ModelWrapper
+  router(srcModule: NodeModule): RouterWrapper
 }
-declare var hot: dvaHot
+
+declare var hot: DvaHot
 export default hot
