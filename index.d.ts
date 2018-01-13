88142ab456d87db1d05b6dcaf188ee5b1ed1b3d6
dgeibi <png.inside@gmail.com>
2018-01-13 18:31:39 +0800

    update api

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,14 @@
 import { Model, DvaInstance, Router } from './dva'
 
+type Container = string | HTMLElement
 type ModelWrapper = (model: Model) => Model
 type RouterWrapper = (router: Router) => Router
 interface NodeModule extends __WebpackModuleApi.Module {}
 interface DvaHot {
-  patch(app: DvaInstance): DvaInstance
+  patch(app: DvaInstance, container?: Container): DvaInstance
   model(srcModule: NodeModule): ModelWrapper
   router(srcModule: NodeModule): RouterWrapper
+  setContainer(container: Container): HTMLElement
 }
 
 declare var hot: DvaHot
